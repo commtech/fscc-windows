7bc387ecfa9cbbcca15a3f440e404c021ed9a24f
William Fagan <willf@commtech-fastcom.com>
2012-10-18 22:25:26 -0500

    Fix a bug in the build_libs script preventing proper building.

--- build_libs.bat
+++ build_libs.bat
@@ -6,7 +6,7 @@ cd %~dp0
 
 :building_driver
 echo Building DLLs...
-pushd %TOP%\c\ & nmake & nmake DEBUG popd > nul
+pushd %TOP%\c\ & nmake & nmake DEBUG & popd > nul
 pushd "%TOP%\c++\" & nmake & nmake DEBUG & popd > nul
 pushd %TOP%\net\ & nmake & nmake DEBUG & popd > nul
 
