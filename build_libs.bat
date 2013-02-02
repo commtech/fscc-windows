96c97929fd2421bde8823b149646cf26f4b2cb36
William Fagan <willf@commtech-fastcom.com>
2013-02-01 19:29:04 -0600

    Fix a comment.

--- build_libs.bat
+++ build_libs.bat
@@ -4,7 +4,7 @@ echo off
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\Tools\VsDevCmd.bat"
 cd %~dp0
 
-:building_driver
+:building_dlls
 echo Building DLLs...
 pushd %TOP%\c\ & nmake clean & nmake & nmake DEBUG & popd > nul
 pushd "%TOP%\c++\" & nmake clean & nmake & nmake DEBUG & popd > nul
