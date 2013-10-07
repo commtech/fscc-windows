74071b7e0ff5df1526c1d72bd2339c171716b095
William Fagan <willf@commtech-fastcom.com>
2013-10-07 12:49:12 -0500

    Revamp release script to rearrange directory structure.

--- release_driver.bat
+++ release_driver.bat
@@ -32,21 +32,37 @@ for %%A in (c, c++, net, python, raw) do mkdir %TOP%\lib\fscc\%%A\
 :copy_dll_files
 echo Copying DLL Files...
 copy lib\raw\*.h %TOP%\lib\fscc\raw\ > nul
+
+mkdir %TOP%\lib\fscc\c\src\
+mkdir %TOP%\lib\fscc\c\docs\
+mkdir %TOP%\lib\fscc\c\examples\
 copy lib\c\cfscc*.dll %TOP%\lib\fscc\c\ > nul
 copy lib\c\cfscc*.lib %TOP%\lib\fscc\c\ > nul
-copy lib\c\src\*.c %TOP%\lib\fscc\c\ > nul
-copy lib\c\src\*.h %TOP%\lib\fscc\c\ > nul
-copy lib\c\makefile %TOP%\lib\fscc\c\ > nul
+copy lib\c\src\*.c %TOP%\lib\fscc\c\src\ > nul
+copy lib\c\src\*.h %TOP%\lib\fscc\c\src\ > nul
+copy lib\c\docs\*.md %TOP%\lib\fscc\c\docs\ > nul
+copy lib\c\examples\*.c %TOP%\lib\fscc\c\examples\ > nul
+
+mkdir "%TOP%\lib\fscc\c++\src\"
+mkdir "%TOP%\lib\fscc\c++\docs\"
+mkdir "%TOP%\lib\fscc\c++\examples\"
 copy "lib\c++\cppfscc*.dll" "%TOP%\lib\fscc\c++\" > nul
 copy "lib\c++\cppfscc*.lib" "%TOP%\lib\fscc\c++\" > nul
+copy "lib\c++\src\*.cpp" "%TOP%\lib\fscc\c++\src\" > nul
+copy "lib\c++\src\*.hpp" "%TOP%\lib\fscc\c++\src\" > nul
+copy "lib\c++\docs\*.md" "%TOP%\lib\fscc\c++\docs\" > nul
+copy "lib\c++\examples\*.cpp" "%TOP%\lib\fscc\c++\examples\" > nul
 copy lib\c\cfscc*.dll "%TOP%\lib\fscc\c++\" > nul
-copy "lib\c++\src\*.cpp" "%TOP%\lib\fscc\c++\" > nul
-copy "lib\c++\src\*.hpp" "%TOP%\lib\fscc\c++\" > nul
-copy "lib\c++\makefile" "%TOP%\lib\fscc\c++\" > nul
+
+mkdir %TOP%\lib\fscc\net\src\
+mkdir %TOP%\lib\fscc\net\docs\
+mkdir %TOP%\lib\fscc\net\examples\
 copy lib\net\netfscc*.dll %TOP%\lib\fscc\net\ > nul
+copy lib\net\src\*.cs %TOP%\lib\fscc\net\src\ > nul
+copy lib\net\docs\*.md %TOP%\lib\fscc\net\docs\ > nul
+copy lib\net\examples\*.cs %TOP%\lib\fscc\net\examples\ > nul
 copy lib\c\cfscc*.dll %TOP%\lib\fscc\net\ > nul
-copy lib\net\src\*.cs %TOP%\lib\fscc\net\ > nul
-copy lib\net\makefile %TOP%\lib\fscc\net\ > nul
+
 xcopy redist\production\serial\lib\* %TOP%\lib\serialfc\ /e /i > nul
 xcopy redist\production\serial\terminal\* %TOP%\terminal\serialfc\ /e /i > nul
 xcopy redist\production\serial\gui\* %TOP%\gui\serialfc\ /e /i > nul
@@ -61,6 +77,10 @@ xcopy lib\c\cfscc.dll %TOP%\lib\fscc\c\loop\ /i > nul
 :copy_example_files
 echo Copying Example Files...
 xcopy examples\*.c %TOP%\examples\fscc\ /e /i > nul
+
+:copy_docs_files
+echo Copying Docs Files...
+xcopy docs\*.md %TOP%\docs\fscc\ /e /i > nul
 
 :copy_test_files
 echo Copying Test Files...
@@ -80,10 +100,6 @@ copy %PYFSCC%\pyfscc*.exe* %TOP%\lib\fscc\python\ > nul
 :echo Copying Setup Files...
 :copy redist\production\i386\dpinst.exe %TOP%\32\setup.exe > nul
 :copy redist\production\amd64\dpinst.exe %TOP%\64\setup.exe > nul
-
-:copy_docs
-echo Copying Docs...
-xcopy lib\c\examples %TOP%\lib\fscc\c\examples /e /i > nul
 
 :copy_changelog
 echo Copying Changelog...
