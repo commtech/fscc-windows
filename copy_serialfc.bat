1fb53954d2d1e898d3032e3083da434d7fa266c5
William Fagan <willf@commtech-fastcom.com>
2013-10-25 11:07:55 -0500

    Update version and ChangeLog for release.

--- copy_serialfc.bat
+++ copy_serialfc.bat
@@ -1,11 +1,11 @@
-set SERIALFC=..\serialfc
+set SERIALFC=..\serialfc\bin\serialfc-windows-2.4.0
 
-copy %SERIALFC%\bin\serialfc\32\serialfc.inf redist\production\i386\serial\ /y
-copy %SERIALFC%\bin\serialfc\32\serialfc.sys redist\production\i386\serial\ /y
+copy %SERIALFC%\32\serialfc.inf redist\production\i386\serial\ /y
+copy %SERIALFC%\32\serialfc.sys redist\production\i386\serial\ /y
 
-copy %SERIALFC%\bin\serialfc\64\serialfc.inf redist\production\amd64\serial\ /y
-copy %SERIALFC%\bin\serialfc\64\serialfc.sys redist\production\amd64\serial\ /y
+copy %SERIALFC%\64\serialfc.inf redist\production\amd64\serial\ /y
+copy %SERIALFC%\64\serialfc.sys redist\production\amd64\serial\ /y
 
-xcopy %SERIALFC%\bin\serialfc\lib\* redist\production\serial\lib\ /e /i /y
-xcopy %SERIALFC%\bin\serialfc\terminal\* redist\production\serial\terminal\ /e /i /y
-xcopy %SERIALFC%\bin\serialfc\gui\* redist\production\serial\gui\ /e /i /y
+xcopy %SERIALFC%\lib\* redist\production\serial\lib\ /e /i /y
+xcopy %SERIALFC%\terminal\* redist\production\serial\terminal\ /e /i /y
+xcopy %SERIALFC%\gui\* redist\production\serial\gui\ /e /i /y
