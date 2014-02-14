3fad85fe067896c34e4c24d575bfd02dc6f2c67e
William Fagan <willf@commtech-fastcom.com>
2014-02-14 15:03:18 -0600

    Fix some error handling in the registry_create_key helper function.

--- src/utils.c
+++ src/utils.c
@@ -83,6 +83,7 @@ NTSTATUS registry_create_key(WDFKEY parent_key, PUNICODE_STRING key_str,
     if (!NT_SUCCESS(status)) {
         TraceEvents(TRACE_LEVEL_ERROR, TRACE_DEVICE,
             "WdfRegistryCreateKey failed %!STATUS!", status);
+        return status;
     }
 
     if (key)
