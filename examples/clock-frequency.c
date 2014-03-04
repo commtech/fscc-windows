d38a10bc4e37faeca58fbaee52d3af1c1639b902
William Fagan <willf@commtech-fastcom.com>
2014-03-04 08:52:26 -0600

    Add calculate_clock_bits to the example code.

--- examples/clock-frequency.c
+++ examples/clock-frequency.c
@@ -4,13 +4,13 @@ int main(void)
 {
     HANDLE h = 0;
     DWORD tmp;
-    char clock_bits[20] = {0xff, 0xff, 0xff, 0x01, 0x84, 0x01, 0x48, 0x72,
-                           0x9a, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                           0x00, 0x04, 0xe0, 0x01};
+    unsigned char clock_bits[20];
     
     h = CreateFile("\\\\.\\FSCC0", GENERIC_READ | GENERIC_WRITE, 0, NULL, 
-                      OPEN_EXISTING, 0, NULL);
-    
+                   OPEN_EXISTING, 0, NULL);
+
+    calculate_clock_bits(10000000, 10, clock_bits);
+
     DeviceIoControl(h, FSCC_SET_CLOCK_BITS, 
                     &clock_bits, sizeof(clock_bits), 
                     NULL, 0, 
