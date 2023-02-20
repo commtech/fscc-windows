50baff954e8aac3b682c11f30303c40615176451
Josh Droney <joshd@commtech-fastcom.com>
2023-02-20 10:54:04 -0600

    Fixed copyright year for fscc.h.

--- lib/raw/fscc.h
+++ lib/raw/fscc.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 Commtech, Inc.
+Copyright 2023 Commtech, Inc.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy 
 of this software and associated documentation files (the "Software"), to deal 
